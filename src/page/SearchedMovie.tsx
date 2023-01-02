9e067024202c349bdfa3a28c0b33ea475b7ce1b2
olaoluwa vincent <olaoluwavincent@gmail.com>
2023-01-02 17:25:18 +0100

    Filter component done

--- src/page/SearchedMovie.tsx
+++ src/page/SearchedMovie.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Movies } from '../../typings';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
@@ -11,53 +11,82 @@ type Props = {
 };
 
 const SearchedMovie = () => {
-	const [filtered, setFiltered] = useState<Movies[]>([]);
-	// Search comes from the CONTEXT state management
 	const { searchContext } = useContext(SeachedContent) as Props;
+	const [movies, setMovies] = useState<Movies[]>([]);
+
+	useEffect(() => {
+		setMovies(searchContext);
+	}, [searchContext]);
+
+	// Search comes from the CONTEXT state management
 	const navigate = useNavigate();
+
+	const filterItem = (curcat: string) => {
+		const newItem = searchContext.filter((newVal) => {
+			return newVal.media_type === curcat;
+			// comparing category for displaying data
+		});
+		setMovies(newItem);
+	};
 
 	// Navigate to the Movie Page
 	const handleMovieClick = (data: Movies) => {
 		navigate('/movie/' + data.id);
 	};
 
-	//Category filters
-	const tv = searchContext.filter((movies) => {
-		if (movies.media_type === 'tv') {
-			return movies;
-		}
-	});
-	const movie = searchContext.filter((movies) => {
-		if (movies.media_type === 'movie') {
-			return movies;
-		}
-	});
+	const filt = [...new Set(searchContext.map((Val) => Val.media_type))];
+
+	const options = () => (
+		<div style={styleDiv}>
+			{filt.map((title) => (
+				<button style={styledButton} onClick={() => filterItem(title)}>
+					{title}
+				</button>
+			))}
+			<button
+				onClick={() => setMovies(searchContext)}
+				style={styledButton}
+			>
+				All{' '}
+				<span
+					style={{
+						background: 'lightgrey',
+						padding: '3px',
+						borderRadius: '50%',
+						fontSize: '70%',
+						color: 'black',
+					}}
+				>
+					{searchContext.length}
+				</span>
+			</button>
+		</div>
+	);
 
 	return (
 		<>
 			<div className='searchPage'>
 				<SearchInput />
-				{searchContext.length < 1 ? (
-					<p style={instruction}>Enter a Search Term</p>
-				) : (
-					CategoriesSelection()
-				)}
 				<div className='searchedMovies'>
-					{searchContext.map((movie) => (
+					{options()}
+					{movies.map((movie) => (
 						<div
 							key={movie.id}
 							className='searchedMovie'
 							onClick={() => handleMovieClick(movie)}
 						>
-							<LazyLoadImage
-								className='searchImage'
-								src={
-									movie.backdrop_path === null
-										? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
-										: `https://image.tmdb.org/t/p/original${movie.poster_path}`
-								}
-								alt={movie.original_title}
-							/>
+							<div className='searchedMovieImage'>
+								<LazyLoadImage
+									className='searchImage'
+									src={
+										movie.backdrop_path === null
+											? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
+											: `https://image.tmdb.org/t/p/original${movie.poster_path}`
+									}
+									alt={movie.original_title}
+								/>
+							</div>
+
 							<p className='searchTitle'>
 								{movie.original_title || movie.original_name}{' '}
 								<br></br> <span>{movie.release_date}</span>
@@ -70,34 +99,26 @@ const SearchedMovie = () => {
 			</div>
 		</>
 	);
-
-	function CategoriesSelection() {
-		return (
-			<div className='searchMovies__categories'>
-				<p onClick={() => setFiltered(searchContext)}>
-					All <span>{searchContext.length}</span>
-				</p>
-				<p style={active} onClick={() => setFiltered(movie)}>
-					Movie <span>{movie.length}</span>
-				</p>
-
-				<p onClick={() => setFiltered(tv)}>
-					Series <span>{tv.length}</span>
-				</p>
-			</div>
-		);
-	}
 };
 
 export default SearchedMovie;
 
-const instruction = {
-	paddingLeft: '8px',
-	color: 'red',
+const styleDiv = {
+	display: 'flex',
+	gap: '30px',
+	width: '100%',
+	paddingLeft: '20px',
 } as React.CSSProperties;
 
-const active = {
-	padding: '5px 8px',
-	background: '#db1619',
-	color: '#ffffff',
+const styledButton = {
+	padding: '8px 16px',
+	background: '#6d6d6ead',
+	outline: 'none',
+	border: 'none',
+	color: 'white',
+	textTransform: 'capitalize',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	gap: '8px',
 } as React.CSSProperties;
